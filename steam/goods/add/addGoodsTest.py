71447d641746670d93bcb933bedb82752d52eb4d
litaojun <78069455@qq.com>
2018-05-13 16:37:35 +0800

    更改数据库

---
+++ steam/goods/add/addGoodsTest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: v1.0
+@author: li.taojun
+@contact: li.taojun@opg.cn
+@site: http://blog.csdn.net/hqzxsc2006
+@software: PyCharm
+@file: addActivityTest.py
+@time: 2018/5/9 15:43
+"""
+from opg.unit.parametrized import ParametrizedTestCase
+from steam.article.add.ArticleAddService import ArticleAddService
+from steam.activity.add.addActivityService import ActivityAddService
+from opg.unit.testcaseRunMgr import runTestOneCls
+
+class GoodsAddTest(ParametrizedTestCase):
+      '''
+            新增文章
+      '''
+      __interfaceName__ = "/steam-resource/admin/product/add-goods"
+      def __init__(self, methodName='runTest', param=None):
+          super(GoodsAddTest,self).__init__(methodName,param)
+          self.inputdata =  self.getInputData()
+          self.expectdata = self.getExpectData()
+          self.activitySer = ActivityAddService(self.inputdata)
+          self.setService(self.activitySer)
+
+      def addGoods(self):
+          addActRsp = self.activitySer.addActivity()
+          retcode = self.activitySer.getRetcodeByActivityRsp(response=addActRsp)
+          self.assertTrue(retcode == self.expectdata["code"])
+
+if __name__ == "__main__":
+   runTestOneCls(
+					casefilepath="\\steamcase\\goods\\goodsaddcase.xlsx",
+					testclse=GoodsAddTest
+				)
---
+++ steam/goods/down/downGoodsTest.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: v1.0
+@author: li.taojun
+@contact: li.taojun@opg.cn
+@site: http://blog.csdn.net/hqzxsc2006
+@software: PyCharm
+@file: downActivityTest.py
+@time: 2018/5/10 17:48
+"""
+
+from opg.unit.parametrized import ParametrizedTestCase
+from steam.article.add.ArticleAddService import ArticleAddService
+from steam.activity.add.addActivityService import ActivityAddService
+from opg.unit.testcaseRunMgr import runTestOneCls
+from steam.activity.search.searchActivityService import ActivitySearchService
+from steam.activity.query.queryActivityService import ActivityQueryService
+from steam.activity.down.downActivityService import ActivityUnPublishService
+
+class GoodsUnPublishTest(ParametrizedTestCase):
+      '''
+            根据ID搜索活动
+      '''
+      __interfaceName__ = "/steam-resource/admin/product/query-activity"
+      def __init__(self, methodName='runTest', param=None):
+          super(GoodsUnPublishTest,self).__init__(methodName,param)
+          self.inputdata =  self.getInputData()
+          self.expectdata = self.getExpectData()
+          self.activitySer = ActivitySearchService(self.inputdata)
+          self.setService(self.activitySer)
+
+      def queryActivityDetail(self):
+          activityRsp = self.activitySer.queryActivity()
+          code = self.activitySer.getRetcodeByActRsp(queryRsp=activityRsp)
+          self.assertTrue(code == self.expectdata["code"])
+          rssid = self.activitySer.getFirstActivityIdByRsp(queryRsp=activityRsp)
+          queryReqJson = {"resourceId":rssid}
+          queryActSer = ActivityUnPublishService(kwargs=queryReqJson)
+          oneActRsp = queryActSer.unPublishActivitySer()
+          code = queryActSer.getRetcodeByDownactRsp(oneActRsp = oneActRsp)
+          self.assertTrue(code == self.expectdata["code"])
+
+
+if __name__ == "__main__":
+          runTestOneCls(
+				          casefilepath="\\steamcase\\goods\\activityunpublishcase.xlsx",
+				          testclse=GoodsUnPublishTest
+			           )
