639f3dd79d17ed73e4ef62c411a7c8fd8857ccb2
opg.li.taojun <li.taojun@opg.cn>
2018-08-28 10:31:07 +0800

    代码优化，增加装饰器

--- steam/user/match/userMatchAppleTest.py
+++ steam/user/match/userMatchAppleTest.py
@@ -28,7 +28,7 @@ class UserMatchAppleTest(SteamTestCase):
           super(UserMatchAppleTest,self).__init__(methodName,param)
 
       def userMatchAppleTest(self):
-          time.sleep(230)
+          time.sleep(70)
           rsp = self.myservice.userMatchApple()
           retcode = self.myservice.getRetcodeByRsp(response=rsp)
           self.assertTrue(retcode == self.expectdata["code"],
