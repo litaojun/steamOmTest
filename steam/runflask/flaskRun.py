7708c3284b4ffa8774cf729567dfd32d5d952717
opg.li.taojun <li.taojun@opg.cn>
2018-08-30 11:21:50 +0800

    代码优化，增加装饰器

--- steam/runflask/flaskRun.py
+++ steam/runflask/flaskRun.py
@@ -17,19 +17,22 @@ from flask import Flask, jsonify,request
 from steam.mediares.query import mediaresQueryTest
 from flask_cors import *
 import sys
+from flask import render_template
 sys.path.append("/home/nicepy/testhome/unittestExBaseb")
 #from opg.unit.testcaseRunMgr import runTest
 from opg.unit.flaskRunMgr import runTest,queryStateByTokenPro,queryTestPlanList,queryPlanDetailByInterfaceName
 import threading
 from opg.unit.flaskRunMgr import getRunTestTokenId,initAllTestCase,initAllTestClass,genAllTestCase,runAllTestCase
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates',static_url_path='/static/')
 CORS(app, supports_credentials=True)
 app.register_blueprint(mediaresQueryTest.bapp,url_prefix="/mediares")
 sign = True
+testSuite = None
 if sign :
     allTestClass = initAllTestClass()
     allTestCase  = initAllTestCase()
-    testSuite    = genAllTestCase(allCase=allTestCase,allTestClass=allTestClass)
+    #testSuite    = genAllTestCase(allCase=allTestCase,allTestClass=allTestClass)
+    tokenList = []
     sign = False
 @app.route('/prop/runtestplan', methods=['GET'])
 def start_tasks():
@@ -53,15 +56,17 @@ def start_tasks():
 def start_steam_tasks():
     projectName = request.args.get("projectname")
     retdata = getRunTestTokenId(projectname=projectName)
+    testSuite = genAllTestCase(allCase=allTestCase, allTestClass=allTestClass)
     t = threading.Thread(target = runAllTestCase,
                          kwargs = {
-                                        "suites" : copy.deepcopy(testSuite),
+                                        "suites" : testSuite,
                                          "title" : projectName,
                                          "description" : "%s-用例测试情况" % projectName,
                                          "token"        :  retdata[0]
                                    }
                          )
     t.start()
+    tokenList.append(retdata[0])
     return jsonify({
                         'sign'  : "000000",
                         "token" : retdata[0],
@@ -86,13 +91,27 @@ def query_plan_CaseRecord():
     return jsonify(queryPlanDetailByInterfaceName(planId=planid))
 
 @app.route('/prop/getOneTestcase', methods=['GET'])
-def query_testCase(interface,methonName,caseId):
+def query_testCase():
+    interface = request.args.get("interface")
+    methonName = request.args.get("methonName")
+    caseId = request.args.get("caseId")
+    print("%s-%s-%s" % (interface,methonName,caseId))
     if interface in allTestCase:
         if methonName in allTestCase[interface]:
             for testcase in allTestCase[interface][methonName]:
                 if testcase[0] == caseId:
-                    return testcase
-    return None
+                    return jsonify(testcase)
+    return "no data"
+
+def stop_test_run():
+    testSuite = None
+    return jsonify({
+                        'code': "000000"
+                   })
+
+@app.route('/')
+def hello_world():
+    return render_template("pytest.html", content="hello flask ")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True,port=8181)
