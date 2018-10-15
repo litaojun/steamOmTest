719288aac5f2e302faafbf4e3949a690a332db42
opg.li.taojun <li.taojun@opg.cn>
2018-10-15 15:48:12 +0800

    代码优化，增加装饰器

--- steam/user/search/weixinSearchEntryTest.py
+++ steam/user/search/weixinSearchEntryTest.py
@@ -21,7 +21,7 @@ class WeixinSearchEntryTest(SteamTestCase):
             微信端用户进入报名页面，获取到赛事，场次，赛题相关信息
       '''
       __interfaceName__ = "/steam-resource/index/configs/searchByEntry"
-      @initInput(services = [ HomeCnfQueryService ],
+      @initInput(services = [ ],
                  curser   = UserSearchEntryService  )
       def __init__(self, methodName = 'runTest',
                          param      = None):
