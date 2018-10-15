5efd0fb2cf086f810f2dbe89386eca99bb3044f4
opg.li.taojun <li.taojun@opg.cn>
2018-10-15 13:59:56 +0800

    代码优化，增加装饰器

--- steam/operposition/query/queryOperpsnService.py
+++ steam/operposition/query/queryOperpsnService.py
@@ -13,7 +13,7 @@ from opg.util.httptools import httpPost,httpGet
 from opg.util.uopService import decorator,UopService
 import requests,json
 from opg.util.utils import query_json
-from steam.util.configurl import queryHomeConfigurl
+from steam.util.configurl import queryArticleurl
 from steam.util.configurl import delArticleurl
 
 class OperpsnQueryService(UopService):
@@ -29,7 +29,7 @@ class OperpsnQueryService(UopService):
                                                   reqjsonfile  = "userOperQueryReq")
 
     def queryOperpsnListdata(self):
-        queryResult = httpGet(url     = queryHomeConfigurl + self.reqjsondata,
+        queryResult = httpGet(url     = queryArticleurl + self.reqjsondata,
                               headers = self.jsonheart)
         return  queryResult
 
