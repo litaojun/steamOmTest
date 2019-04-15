290f51b5343590f3fbee4b0040a5fe9ccd455d9b
opg.li.taojun <li.taojun@opg.cn>
2019-04-15 17:39:13 +0800

    增加mitmproxy

---
+++ steam/mtpy/steamMtpy.py
@@ -0,0 +1,33 @@
+import typing
+
+import mitmproxy.addonmanager
+import mitmproxy.connections
+import mitmproxy.http
+import mitmproxy.log
+import mitmproxy.tcp
+import mitmproxy.websocket
+import mitmproxy.proxy.protocol
+
+
+class SteamMtyp:
+
+    def request(self, flow: mitmproxy.http.HTTPFlow):
+        """
+            The full HTTP request has been read.
+        """
+        method = flow.request.method
+        if method == "GET":
+            query = flow.request.query
+            queryurl = "&".join(["%s=%s" % (k,v) for k,v in query.items()])
+            print("query = %s " % queryurl)
+        elif method == "POST":
+            body = flow.request.get_text()
+            print("body = %s" % body)
+
+
+    def response(self, flow: mitmproxy.http.HTTPFlow):
+        """
+            The full HTTP response has been read.
+        """
+        data = flow.response.text
+        print("body = %s" % data)
