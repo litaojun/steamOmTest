b3e3cf30d21ffcf6066e096fc9e496ecc7312478
opg.li.taojun <li.taojun@opg.cn>
2019-04-15 17:53:18 +0800

    增加mitmproxy

--- steam/mtpy/steamMtpy.py
+++ steam/mtpy/steamMtpy.py
@@ -16,6 +16,7 @@ class SteamMtyp:
             The full HTTP request has been read.
         """
         method = flow.request.method
+        print("litaojun - get")
         if method == "GET":
             query = flow.request.query
             queryurl = "&".join(["%s=%s" % (k,v) for k,v in query.items()])
