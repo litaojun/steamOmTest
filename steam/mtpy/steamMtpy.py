15623a42308ceec56a1a7a80594bcddf26ac6dfb
opg.li.taojun <li.taojun@opg.cn>
2019-04-16 10:04:16 +0800

    mitmproxy增加host过滤

--- steam/mtpy/steamMtpy.py
+++ steam/mtpy/steamMtpy.py
@@ -16,23 +16,29 @@ class SteamMtyp:
             The full HTTP request has been read.
         """
         method = flow.request.method
-        print("itaojun - gelt")
-        ctx.log.info("itaojun - gelt")
-        if method == "GET":
-            query = flow.request.query
-            queryurl = "&".join(["%s=%s" % (k,v) for k,v in query.items()])
-            print("query = %s " % queryurl)
-        elif method == "POST":
-            body = flow.request.get_text()
-            print("body = %s" % body)
+        host = flow.request.host
+        print("SteamMtyp - request method=%s,host=%s" % (method,host))
+        if host == "uat-steam-api.opg.cn":
+            ctx.log.info("itaojun - gelt")
+            if method == "GET":
+                query = flow.request.query
+                queryurl = "&".join(["%s=%s" % (k,v) for k,v in query.items()])
+                print("query = %s " % queryurl)
+            elif method == "POST":
+                body = flow.request.get_text()
+                print("body = %s" % body)
 
 
     def response(self, flow: mitmproxy.http.HTTPFlow):
         """
             The full HTTP response has been read.
         """
-        data = flow.response.text
-        print("body = %s" % data)
+        method = flow.request.method
+        host = flow.request.host
+        print("SteamMtyp - request method=%s,host=%s" % (method,host))
+        if host == "uat-steam-api.opg.cn":
+            data = flow.response.text
+            print("body = %s" % data)
 
 
 def start():
