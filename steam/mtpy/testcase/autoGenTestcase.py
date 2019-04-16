a7573e845f5a41375beef51ffedcd186a87f54ea
opg.li.taojun <li.taojun@opg.cn>
2019-04-16 18:30:32 +0800

    yaml操作放util

--- steam/mtpy/testcase/autoGenTestcase.py
+++ steam/mtpy/testcase/autoGenTestcase.py
@@ -1,3 +1,22 @@
 from steam.mockhttp.flaskHttpServer import httpData
+from steam.util.configIni import basePath
+from opg.util.yamlOper import readYmlFile,dumpDataToYmlFile
+import os
+def loadCaseTmpFile():
+    filepath = os.sep.join([basePath,"template","testcaseMitmproxy.template"])
+    caseTmpData = readYmlFile(filePath=filepath)
+    return caseTmpData
+
+def getCaseTmpFilepath():
+    filepath = os.sep.join([basePath,"template","testcaseMitmproxy.template"])
+    return filepath
+
+def getTestcasePath(usertype,modul):
+    filePath = os.sep.join([basePath,"mitmproxy","steam",usertype,modul])
+    return filePath
+
 def genAutoCase(method=None,host=None,url=None,path=None,reqbody=None):
-    pass
+    usertype,modul = httpData[path][5],httpData[path][3]
+    caseTmpDataDict = loadCaseTmpFile()
+    
+
