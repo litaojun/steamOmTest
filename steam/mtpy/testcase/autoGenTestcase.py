3f0b3e6c3741b46bd249fc2380731a5d7c1f4aec
opg.li.taojun <li.taojun@opg.cn>
2019-04-17 18:46:53 +0800

    增加写req json到文件

--- steam/mtpy/testcase/autoGenTestcase.py
+++ steam/mtpy/testcase/autoGenTestcase.py
@@ -1,6 +1,7 @@
 from steam.mockhttp.flaskHttpServer import httpData
 from steam.util.configIni import basePath
 from opg.util.yamlOper import readYmlFile,dumpDataToYmlFile
+from steam.util.formatJsonFile import writeStrToJsonFile
 import os
 from mitmproxy import ctx
 
@@ -11,17 +12,26 @@ def loadCaseTmpFile():
     ctx.log.info("loadCaseTmpFile - caseTmpData=%s" % caseTmpData)
     return caseTmpData
 
+#写yaml测试用例到文件
 def dumpYmalCaseToFile(path,usertype,modul,testcase):
     filePath = getTestcasePath(usertype=usertype,modul=modul,path=path)
     dumpDataToYmlFile(filePath=filePath,data=testcase)
 
-
-def getTestcasePath(usertype,modul,path):
+#获取测试用例的路径OR请求响应数据格式路径
+def getTestcasePath(usertype,modul,path,fileType="steamcase"):
+    """
+    :param usertype: 用户类型，一级目录，admin,weixin,merchants
+    :param modul: 二级目录，模块级别，与testjson-url.yml配置一致
+    :param path:  请求URL标识
+    :param fileType: steamcase:测试用例目录， steam:请求响应数据目录
+    :return:
+    """
     fileName = "".join([ name.capitalize() if index>1 else name for index,name in enumerate(path.split("/"))]) + "s.yml"
-    filePath = os.sep.join([basePath,"mitmproxy","steamcase",usertype,modul,fileName])
+    filePath = os.sep.join([basePath,"mitmproxy",fileType,usertype,modul,fileName])
     print("filePath = %s" % filePath)
     return filePath
 
+#生成自动化测试用例
 def genAutoCase(method=None,host=None,url=None,path=None,reqbody=None):
     ctx.log.info("genAutoCase,path=%s" % path)
     if path not in httpData:
@@ -43,6 +53,16 @@ def genAutoCase(method=None,host=None,url=None,path=None,reqbody=None):
     filePath = getTestcasePath(usertype=usertype,modul=modul,path=path)
     ctx.log.info("filePat=%s,caseData=%s" %(filePath,caseTmpDataDict))
     dumpDataToYmlFile(filePath=filePath,data=caseTmpDataDict)
+
+#生成请求数据
+def genReqData(method=None,host=None,url=None,path=None,reqbody=None):
+    ctx.log.info("genReqData,path=%s" % path)
+    if path not in httpData:
+        print("path=%s is not exist in testjson-url.yml" % path)
+        return
+    usertype, modul, title = httpData[path][5], httpData[path][7], httpData[path][4]
+    reqFilePath = getTestcasePath(usertype=usertype,modul=modul,path=path,fileType="steam")
+    writeStrToJsonFile(filePath=reqFilePath,jsonStr=reqbody,rwmode="a+")
 
 if __name__ == "__main__":
     testData = {
